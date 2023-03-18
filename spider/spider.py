1479b284442c70419b1e97e406ba4352063fac24
Bojie Li <bojieli@gmail.com>
2023-03-18 23:43:42 +0800

    insert into db

--- spider/spider.py
+++ spider/spider.py
@@ -1,14 +1,43 @@
 import scrapy
 import re
+import pymysql
+import datetime    
 
 domain = 'ustc.edu.cn'
+
 mysql_db = 'spider_ustc'
 mysql_host = 'localhost'
+mysql_user = 'root'
+mysql_password = ''
 mysql_table = 'webpages'
 
+db_conn = pymysql.connect(mysql_host, mysql_user, mysql_password, mysql_db, cursorclass=pymysql.cursors.DictCursor)
+if not db_conn:
+    print('database connection failed')
+
+
 def save_webpage(response):
-    print(response.url)
-    return
+    with db_conn.cursor() as cursor:
+        curr_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        sql = "INSERT INTO " + mysql_table + " (url, data, crawl_time) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (response.url, response.data, curr_time))
+        db_conn.commit()
+        return {'url': response.url, 'time': curr_time }
+    return None
+
+
+def should_crawl(url):
+    # check whether the URL is in a subdomain of the target website
+    if not re.match('https?://([a-z0-9.-]+\.)?' + domain + '/', url):
+        return False
+    # check whether the URL has been crawled
+    with db_conn.cursor() as cursor:
+        sql = "SELECT crawl_time FROM " + mysql_table + " WHERE url = %s"
+        cursor.execute(sql, (url,))
+        if cursor.fetchone():
+            return False
+    return True
+
 
 class USTCSpider(scrapy.Spider):
     name = 'ustc-spider'
@@ -19,8 +48,8 @@ class USTCSpider(scrapy.Spider):
             return
         if not response.headers['Content-Type'].startswith(b'text/'):
             return
-        save_webpage(response)
+        yield save_webpage(response)
 
         for next_page in response.css('a::attr(href)'):
-            if re.match('https?://([a-z0-9.-]+\.)?' + domain + '/', next_page.get()):
+            if should_crawl(next_page.get()):
                 yield response.follow(next_page, self.parse)
