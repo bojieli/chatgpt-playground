baf338112d8f34de53a4007b7a34878a9adee574
Bojie Li <bojieli@gmail.com>
2023-03-24 15:24:04 +0800

    a simplistic openai API test

---
+++ chatbot/test-openai-api.py
@@ -0,0 +1,14 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+if os.getenv("HTTP_PROXY"):
+    openai.proxy = os.getenv("HTTP_PROXY")
+
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "What is the last human light if all humans suddenly disappear at the same time?"}
+  ]
+)
+print(response)
