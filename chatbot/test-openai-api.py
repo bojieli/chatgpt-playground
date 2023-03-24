910d3ff2eee415ad2c04c16e2fcf678cd9d0aaa4
Bojie Li <bojieli@gmail.com>
2023-03-24 17:20:40 +0800

    force setting HTTP proxy to avoid direct connect

--- chatbot/test-openai-api.py
+++ chatbot/test-openai-api.py
@@ -3,7 +3,9 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 if os.getenv("HTTP_PROXY"):
-    openai.proxy = os.getenv("HTTP_PROXY")
+    openai.proxy = os.getenv("HTTPS_PROXY")
+else:
+    raise ValueError('HTTP_PROXY environment variable is not set')
 
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
